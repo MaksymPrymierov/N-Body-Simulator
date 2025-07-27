11b5311fda261d8eae8be6bf564ce9ef5cb1b2d1
Maksym Prymierov <primerovmax@gmail.com>
2025-07-27 17:45:32 +0300

    [NBS-36] feat(cli): add support for output mode and file writing
    - Replaced boolean `output` flag with a string option (e.g., `none`,
    `plain_text`)
    - Added `--file` CLI flag to redirect output to `output.txt`
    - Modified `NBodySystem` to support formatted plain text output
    - Updated `.gitignore` to exclude `output.txt`
    
    Signed-off-by: Maksym Prymierov <primerovmax@gmail.com>

--- src/terminal/cli.rs
+++ src/terminal/cli.rs
@@ -1,18 +1,23 @@
 use crate::types::nbodysystem::NBodySignal;
 use crate::types::nbodysystem::NBodySystem;
+use crate::types::particle::Particle;
 use clap::Parser;
 
 /// N Body Problem
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    /// Print the velocities and coordinates
-    #[arg(short, long)]
-    output: bool,
+    /// Select the type of data output
+    #[arg(short, long, default_value = "none", value_parser = [ "none", "terminal", "plain_text"])]
+    output: String,
 
     /// Limit the output of information about particles to once every N steps
     #[arg(short, long, default_value_t = 1000)]
     limit: u16,
+
+    /// Set a file name to save the results
+    #[arg(short, long, default_value = "output.txt")]
+    file: String,
 }
 
 pub struct NBodyCli<'a> {
@@ -30,26 +35,57 @@ impl<'a> NBodyCli<'a> {
 
     pub async fn handle_args(&mut self) {
         self.m_n_body_system.set_limit(self.m_args.limit);
+        let mut receiver = self.m_n_body_system.subscribe();
+        let file_name = self.m_args.file.clone();
+        let output_type = self.m_args.output.clone();
 
-        if self.m_args.output {
-            let mut receiver = self.m_n_body_system.subscribe();
-
-            std::thread::spawn(move || {
-                while let Ok(signal) = receiver.blocking_recv() {
-                    match signal {
-                        NBodySignal::LimitReached { particles } => {
-                            for particle in particles {
-                                println!(
-                                    "Particle: [{}] POS: {:?}; Velocity: {:?};",
-                                    particle.id(),
-                                    particle.pos(),
-                                    particle.velocity()
-                                );
-                            }
+        std::thread::spawn(move || {
+            while let Ok(signal) = receiver.blocking_recv() {
+                match signal {
+                    NBodySignal::LimitReached { particles } => match output_type.as_str() {
+                        "none" => {}
+                        "terminal" => {
+                            let writer = create_terminal_writer();
+                            writer(&particles);
                         }
-                    }
+                        "plain_text" => {
+                            let writer = create_plain_text_writer();
+                            writer(&particles, &file_name);
+                        }
+                        _ => println!("Incorrect output type"),
+                    },
                 }
-            });
+            }
+        });
+    }
+}
+
+fn create_terminal_writer() -> impl Fn(&Vec<Particle>) + Send {
+    move |particles: &Vec<Particle>| {
+        for particle in particles {
+            println!(
+                "Particle: [{}] POS: {:?}; Velocity: {:?};",
+                particle.id(),
+                particle.pos(),
+                particle.velocity()
+            );
         }
+    }
+}
+
+fn create_plain_text_writer() -> impl Fn(&Vec<Particle>, &String) + Send {
+    move |particles: &Vec<Particle>, file_name: &String| {
+        let mut output = String::new();
+        println!("Write to file: {file_name}");
+        for particle in particles {
+            output += format!(
+                "Particle: [{}] POS: {:?}; Velocity: {:?};\n",
+                particle.id(),
+                particle.pos(),
+                particle.velocity()
+            )
+            .as_str();
+        }
+        std::fs::write(file_name.as_str(), output).expect("Unable to write to file");
     }
 }
